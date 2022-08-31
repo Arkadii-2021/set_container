2aabcc1939a291c13cc62aa201e7f32228e88c3e
arkadii <88792842+Arkadii-2021@users.noreply.github.com>
2022-08-31 17:05:39 +0700

    change the tests

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -4,38 +4,27 @@ import {
   Bowerman, Swordsman, Magician, Daemon, Undead, Zombie,
 } from '../characterRepo';
 
-const bowerman = new Bowerman();
-const swordsman = new Swordsman();
-const magician = new Magician();
-const magician3 = new Magician();
-const daemon = new Daemon();
-const undead = new Undead();
-const zombie = new Zombie();
+const bowerman = new Bowerman().type;
+const swordsman = new Swordsman().type;
+const magician = new Magician().type;
+const daemon = new Daemon().type;
+const undead = new Undead().type;
+const zombie = new Zombie().type;
 
 const teamHeroes = new Team();
 
 teamHeroes.add(bowerman);
 teamHeroes.add(swordsman);
 teamHeroes.add(magician);
-teamHeroes.add(magician3);
 
 teamHeroes.addAll(daemon, undead, zombie);
+teamHeroes.addAll(daemon, undead, zombie);
 
-const arrCheck = [
-  {
-    name: 'bowerman',
-    health: 100,
-    level: 1,
-    type: 'Bowerman',
-    attack: 25,
-    defence: 25,
-  },
-];
-
-test('add character in Team', () => {
-  expect(teamHeroes.Bowerman).toEqual(arrCheck.Bowerman);
-});
 
 test('set Team to Array', () => {
   expect(teamHeroes.toArray()).toEqual(expect.arrayContaining(teamHeroes.toArray()));
+});
+
+test('Size in Set', () => {
+  expect(teamHeroes.toArray().length).toBe(6);
 });
