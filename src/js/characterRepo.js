6c4b11dd2db47b4eac001ff58fd895cd761be094
arkadii <88792842+Arkadii-2021@users.noreply.github.com>
2022-08-31 12:28:34 +0700

    fix set-сontainer -hw

--- src/js/characterRepo.js
+++ src/js/characterRepo.js
@@ -1,55 +1,53 @@
 export class Character {
-  constructor(name) {
-    this.name = name;
+  constructor() {
     this.health = 100;
     this.level = 1;
-    if (name === null || typeof name !== 'string' || (name.length <= 2 || name.length > 10)) throw new Error();
   }
 }
 
 export class Bowerman extends Character {
-  constructor(name, health, level) {
-    super(name);
+  constructor(health, level) {
+	super();
     this.type = 'Bowerman';
     this.attack = 25;
     this.defence = 25;
   }
 }
 export class Swordsman extends Character {
-  constructor(name, health, level) {
-    super(name);
+  constructor(health, level) {
+	super();
     this.type = 'Swordsman';
     this.attack = 40;
     this.defence = 10;
   }
 }
 export class Magician extends Character {
-  constructor(name, health, level) {
-    super(name);
+  constructor(health, level) {
+	super();
     this.type = 'Magician';
     this.attack = 10;
     this.defence = 40;
   }
 }
 export class Daemon extends Character {
-  constructor(name, health, level) {
-    super(name);
+  constructor(health, level) {
+	super();
     this.type = 'Daemon';
     this.attack = 10;
     this.defence = 40;
   }
 }
 export class Undead extends Character {
-  constructor(name, health, level) {
-    super(name);
+  constructor(health, level) {
+	super();
     this.type = 'Undead';
     this.attack = 25;
     this.defence = 25;
   }
 }
 export class Zombie extends Character {
-  constructor(name, health, level) {
-    super(name);
+  constructor(health, level) {
+    super();
     this.type = 'Zombie';
     this.attack = 40;
     this.defence = 10;
